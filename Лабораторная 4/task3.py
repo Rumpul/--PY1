cf757431cd8293fae8e0dbccf1494a0179b01fd3
Rumpul <109575766+Rumpul@users.noreply.github.com>
2022-10-30 21:43:32 +0300

    Add files via upload

--- Лабораторная 4/task3.py
+++ Лабораторная 4/task3.py
@@ -13,3 +13,4 @@ def delete(list_, index=None):
 print(delete([0, 0, 1, 2], index=0))  # [0, 1, 2]
 print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2, 3]
 print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
+print(delete([1, 2, 3], index=-1)) # [1, 2]
