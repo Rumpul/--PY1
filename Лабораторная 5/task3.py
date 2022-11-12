df34cfdfa9e80fca889631c62d34e9cddfe569af
Rumpul <109575766+Rumpul@users.noreply.github.com>
2022-11-12 19:10:09 +0300

    Add files via upload

--- Лабораторная 5/task3.py
+++ Лабораторная 5/task3.py
@@ -1,13 +1,19 @@
 import random
-def get_unique_list_numbers(start = -10, stop = 11, size = 15) -> list[int]:
-    list_numbers = list(range(start, stop))  # TODO написать функцию для получения списка уникальных целых чисел
-    random.shuffle(list_numbers)
-    list_ = []
-    for value in list_numbers:
-            list_.append(value)
-    return list_[:size]
+
+def get_unique_list_numbers(start=-10, stop=11, size=15) -> list[int] :
+      # TODO написать функцию для получения списка уникальных целых чисел
+    if size <= len(range(start, stop)):
+        list_numbers = []
+        while len(list_numbers) < size:
+            value = random.randint(start, stop)
+            if value not in list_numbers:
+                list_numbers.append(value)
+        return list_numbers
+    else:
+        raise ValueError('Заданный размер списка больше, чем количество чисел в заданном диапазоне')
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers))
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
+
