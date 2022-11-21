8959357a13560dec41d2801c6697aa10ed29d6c5
Rumpul <109575766+Rumpul@users.noreply.github.com>
2022-11-21 19:38:45 +0300

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -1,21 +1,37 @@
-import json
-
-
 INPUT_FILE = "input.csv"
 OUTPUT_FILE = "output.json"
 
 
 def csv_to_list_dict(filename, delimiter=',', newline='\n'):
     with open(filename) as input_file:
-        json_data = []
-        headings = input_file.readline().rstrip().split(delimiter)
+        headings = input_file.readline().replace('\n', "").split(delimiter)
         for line in input_file:
-            input_line = line.strip().split(newline)
+            input_line = line.replace('\n', "").split(newline)
             for current_line in input_line:
-                json_data.append(dict(zip(headings, current_line.split(delimiter))))
-        yield json_data
+                yield dict(zip(headings, current_line.split(delimiter)))
 
 
-with open(OUTPUT_FILE, 'w') as output_file:
-    for output_data in csv_to_list_dict(INPUT_FILE):
-        json.dump(output_data, output_file, indent=4)
+def json_writer(filename, delimiter=',', newline='\n', indent=' '):
+    line_count = sum(1 for line in open(INPUT_FILE))-1
+    with open(filename, 'w') as output_file:
+        output_file.write('[' + newline)
+        count_output_file_lines = 0
+        for output_data in csv_to_list_dict(INPUT_FILE):
+            output_file.write(indent + '{' + newline)
+            count_output_data = 0
+            for key, value in output_data.items():
+                output_file.write(f'{indent}{indent}"{key}": {value}')
+                if count_output_data < len(output_data)-1:
+                    output_file.write(delimiter + newline)
+                else:
+                    output_file.write(newline)
+                count_output_data += 1
+            count_output_file_lines += 1
+            if count_output_file_lines < line_count:
+                output_file.write(indent + '}' + delimiter + newline)
+            else:
+                output_file.write(indent + '}' + newline)
+        output_file.write(']')
+
+
+json_writer(OUTPUT_FILE)
