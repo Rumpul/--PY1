107fa76353b7256d1de3952725ece458b03b53f0
Rumpul <109575766+Rumpul@users.noreply.github.com>
2022-11-22 14:38:39 +0300

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -12,26 +12,20 @@ def csv_to_list_dict(filename, delimiter=',', newline='\n'):
 
 
 def json_writer(filename, delimiter=',', newline='\n', indent='     '):
-    line_count = sum(1 for line in open(INPUT_FILE))-1
     with open(filename, 'w') as output_file:
-        output_file.write('[' + newline)
-        count_output_file_lines = 0
-        for output_data in csv_to_list_dict(INPUT_FILE):
-            output_file.write(indent + '{' + newline)
-            count_output_data = 0
-            for key, value in output_data.items():
-                output_file.write(f'{indent}{indent}"{key}": {value}')
-                if count_output_data < len(output_data)-1:
-                    output_file.write(delimiter + newline)
-                else:
-                    output_file.write(newline)
-                count_output_data += 1
-            count_output_file_lines += 1
-            if count_output_file_lines < line_count:
-                output_file.write(indent + '}' + delimiter + newline)
+        output_file.write('[')
+        for count_output_file_lines, output_data in enumerate(csv_to_list_dict(INPUT_FILE)):
+            if count_output_file_lines == 0:
+                output_file.write(newline + indent + '{' )
             else:
-                output_file.write(indent + '}' + newline)
-        output_file.write(']')
+                output_file.write(delimiter + newline + indent + '{')
+            for count_output_data, (key, value) in enumerate(output_data.items()):
+                if count_output_data == 0:
+                    output_file.write(newline + f'{indent}{indent}"{key}": {value}')
+                else:
+                    output_file.write(delimiter + newline + f'{indent}{indent}"{key}": {value}')
+            output_file.write(newline + indent + '}')
+        output_file.write(newline + ']')
 
 
 json_writer(OUTPUT_FILE)
